7f55b88c0617c343d388de6eb44f39c26b84b152
delta <some@email.org>
2016-03-31 05:38:54 -0500

    tweaks + max jobs

--- sync-arcade.js
+++ sync-arcade.js
@@ -5,7 +5,10 @@ var util = require('util');
 var exec = require('child_process').exec;
 
 var deltaPath = '/mnt/delta1/arcade';
-var arcadePath = '/home/pi/RetroPie/roms';
+var arcadePath = '/home/player/RetroPie';
+var jobCounter =0;
+var currentJobs = 0;
+var maxJobs = 5;
 
 watch(deltaPath, function(filename){
 	var file = filename.split(deltaPath)[1];
@@ -13,16 +16,28 @@ watch(deltaPath, function(filename){
 		   .replace(/\'/g, "\\'").replace(/\"/g, '\\"').replace(/\$/g, '\\$');
 	var fileExists = fs.existsSync(filename);
 	var cmd = "";
+	var job = ++jobCounter;
 
 	if(fileExists){ //not a deletion
-		cmd = 'scp -pr "' + filename + '" ' + 'pi@10.0.0.13:"' + arcadePath + file + '"';
-		console.log('Add/Sync: ', cmd);
+		cmd = 'scp -pr "' + filename + '" ' + 'player@10.0.0.13:"' + arcadePath + file + '"';
+		console.log('Job:'+job+'\('+currentJobs+'//'+maxJobs+'\) Add/Sync: ', cmd);
 	}else{ //is a deletion
-		cmd = 'ssh pi@10.0.0.13 "rm -r ' + arcadePath + file + '"';
-		console.log('Delete: ', cmd);
+		cmd = 'ssh player@10.0.0.13 "rm -r ' + arcadePath + file + '"';
+		console.log('Job:'+job+'\('+currentJobs+'//'+maxJobs+'\) Delete: ', cmd);
 	}
-	var child = exec(cmd, function(error, stdout, stderr){
-		if(error) console.log("Error: " + error);
-		else console.log('Synced! ' + stdout);
-	});
+	checkThenRun(cmd, job);
 });
+
+function checkThenRun(cmd, job){
+	if(currentJobs >= maxJobs){
+		console.log("Warning: Max number of current jobs met, waiting 200ms");
+		setTimeout(function(){checkThenRun(cmd, job);}, 200);
+	}else{
+		currentJobs++;
+		var child = exec(cmd, function(error, stdout, stderr){ 
+                	if(error) console.log('Job:'+job+' Error: ' + error);
+                	else console.log('Job:'+job+' Synced! ' + stdout);
+        		currentJobs--;
+		});
+	}
+}
